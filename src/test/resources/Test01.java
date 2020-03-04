8de3d62373a69ad9db41d1d94ddba6470627a0db
Willyams Yujra <yracnet@gmail.com>
2020-03-04 11:17:49 -0400

    * refactor maven structure directory

---
+++ src/test/resources/Test01.java
@@ -0,0 +1,11 @@
+
+public class Test01 {
+        public static String main (String args){
+            call("c");
+            return "xxx";
+        }
+        
+        public static String call(String val){
+            return "xxx" + val;
+        }
+}
